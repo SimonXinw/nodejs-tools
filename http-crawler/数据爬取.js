4fade31ea4e72f1921f95e4861f345cbfd0ae159
wb-xw818042 <wb-xw818042@alibaba-inc.com>
2023-07-28 16:03:34 +0800

    feat： 能出来，但是调试有问题

--- http-crawler/数据爬取.js
+++ http-crawler/数据爬取.js
@@ -1,68 +1,48 @@
-import axios from 'axios'
-import fs from 'fs'
-import cheerio from "cheerio"
 import puppeteer from "puppeteer"
-
 import { COOKIES_ARR } from "./const.js"
 
 // URL of the page we want to scrape
 const url = "https://pre-marvel.alibaba-inc.com/#/ceres/grouping/seller";
 
 // Async function which scrapes the data
-async function scrapeData() {
+async function scrapeData(config) {
+
+  const url = config?.url;
+
   try {
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: "new" });
+
     const page = await browser.newPage();
 
-    // 添加Cookie
+
 
     // 转换Cookie格式并设置到Puppeteer中
     for (const cookie of COOKIES_ARR) {
       await page.setCookie(cookie);
     }
 
-    await page.goto('https://pre-marvel.alibaba-inc.com/#/ceres/grouping/seller');
-
-    // 等待页面加载完毕
-    await page.waitForNavigation({ waitUntil: 'networkidle0' });
+    await page.goto(url);
 
     // 等待某个元素的出现
-    await page.waitForSelector('.next-menu-item-inner');
+    await page.waitForSelector('.next-aside-navigation');
 
-    // 等待一段时间
-    await page.waitForTimeout(5000);
+    //下面写法等价
+    let selectedHeadADom = await page.$('.next-shell-header .next-menu-selectable-single .next-selected a');
 
-    // Load HTML we fetched in the previous line
-    const $ = cheerio.load(data);
-    // Select all the list items in plainlist class
-    const listItems = $(".plainlist ul li");
-    // Stores data for all countries
-    const countries = [];
-    // Use .each method to loop through the li we selected
-    listItems.each((idx, el) => {
-      // Object holding data for each country/jurisdiction
-      const country = { name: "", iso3: "" };
-      // Select the text content of a and span elements
-      // Store the textcontent in the above object
-      country.name = $(el).children("a").text();
-      country.iso3 = $(el).children("span").text();
-      // Populate countries array with country data
-      countries.push(country);
-    });
-    // Logs countries array to the console
-    console.dir(countries);
-    // Write countries array in countries.json file
-    fs.writeFile("coutries.json", JSON.stringify(countries, null, 2), (err) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log("Successfully written data to file");
-    });
+    console.log('selectedHeadADom >>>>>>>>>>>>>>', selectedHeadADom)
+
+    let STR_1 = selectedHeadADom.innerText + ' - ';
+
+    // 关闭浏览器
+    await browser.close();
+
   } catch (err) {
     console.error(err);
   }
 }
 // Invoke the above function
-scrapeData();
+scrapeData({
+
+  url: url
+});
