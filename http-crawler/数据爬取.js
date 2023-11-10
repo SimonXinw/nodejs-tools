e03901cc17d3bd6aba7450d43077a9919d67ebb1
xinwang <wb-xw818042@alibaba-inc.com>
2023-11-10 17:04:13 +0800

    feat: 提取文件夹中的文件

--- http-crawler/数据爬取.js
+++ http-crawler/数据爬取.js
@@ -1,21 +1,17 @@
-import puppeteer from "puppeteer"
-import { COOKIES_ARR } from "./const.js"
+import puppeteer from 'puppeteer';
+import { COOKIES_ARR } from './const.js';
 
 // URL of the page we want to scrape
-const url = "https://pre-marvel.alibaba-inc.com/#/ceres/grouping/seller";
+const url = 'https://pre-marvel.alibaba-inc.com/#/ceres/grouping/seller';
 
 // Async function which scrapes the data
 async function scrapeData(config) {
-
   const url = config?.url;
 
   try {
-
-    const browser = await puppeteer.launch({ headless: "new" });
+    const browser = await puppeteer.launch({ headless: 'new' });
 
     const page = await browser.newPage();
-
-
 
     // 转换Cookie格式并设置到Puppeteer中
     for (const cookie of COOKIES_ARR) {
@@ -28,21 +24,21 @@ async function scrapeData(config) {
     await page.waitForSelector('.next-aside-navigation');
 
     //下面写法等价
-    let selectedHeadADom = await page.$('.next-shell-header .next-menu-selectable-single .next-selected a');
+    let selectedHeadADom = await page.$(
+      '.next-shell-header .next-menu-selectable-single .next-selected a'
+    );
 
-    console.log('selectedHeadADom >>>>>>>>>>>>>>', selectedHeadADom)
+    console.log('selectedHeadADom >>>>>>>>>>>>>>', selectedHeadADom);
 
     let STR_1 = selectedHeadADom.innerText + ' - ';
 
     // 关闭浏览器
     await browser.close();
-
   } catch (err) {
     console.error(err);
   }
 }
 // Invoke the above function
 scrapeData({
-
-  url: url
+  url: url,
 });
